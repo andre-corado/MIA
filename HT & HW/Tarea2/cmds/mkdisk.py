476fcce749548fa2e9216716280a45a7f3f27454
andre-corado <98116674+andre-corado@users.noreply.github.com>
2023-08-28 20:56:35 -0600

    partition struct

--- HT & HW/Tarea2/cmds/mkdisk.py
+++ HT & HW/Tarea2/cmds/mkdisk.py
@@ -10,7 +10,7 @@ def execute(consoleLine):
     return "Comando mkdisk ejecutado.\n"
 
 
-class MBR:
+class MBR: # Size = 136 bytes
     def __init__(self, size, fit='F'):
         self.mbr_tamano = size
         self.mbr_fecha_creacion = ""
@@ -22,8 +22,14 @@ class MBR:
         self.mbr_partition_4 = None
     
     # ------------ STRUCTURE ------------
-    # tamano-4 | fecha-19 | signature-4 | fit-1 
-    
+    # tamano-4 | fecha-19 | signature-4 | fit-1 | 
+    # <-- 28 bytes --> | DATA
+
+    # partion_1-27 | partition_2-27 | partition_3-27 | partition_4-27
+    # <-- 108 bytes -->
+
+    # TOTAL = 136 bytes
+
     def encode(self):
         bytes = bytearray()
         bytes += self.mbr_tamano.to_bytes(4, byteorder='big')
@@ -53,3 +59,43 @@ class MBR:
             file.write(self.encode())
         file.close()
         print("Disco creado exitosamente.")
+
+
+class Partition: # Size = 27 bytes
+    def __init__(self, status='0', type='P', fit='F', start=-1, size=0, name=''):
+        self.part_status = status # Char 
+        self.part_type = type # Char P o E 
+        self.part_fit = fit # Char B, F o W
+        self.part_start = start # Int4 signed
+        self.part_s = size # Int4
+        self.part_name = formatStr(name, 16)
+
+
+    # ------------ STRUCTURE ------------
+    # status-1 | type-1 | fit-1 | start-4 | size-4 | name-16
+    # <-- 27 bytes -->
+
+    def encode(self):
+        bytes = bytearray()
+        bytes += self.part_status.encode()
+        bytes += self.part_type.encode()
+        bytes += self.part_fit.encode()
+        bytes += self.part_start.to_bytes(4, byteorder='big', signed=True)
+        bytes += self.part_s.to_bytes(4, byteorder='big')
+        bytes += self.part_name.encode()
+        return bytes
+    
+    def decode(self, bytes):
+        self.part_status = bytes[0:1].decode()
+        self.part_type = bytes[1:2].decode()
+        self.part_fit = bytes[2:3].decode()
+        self.part_start = int.from_bytes(bytes[3:7], byteorder='big', signed=True)
+        self.part_s = int.from_bytes(bytes[7:11], byteorder='big')
+        self.part_name = bytes[11:27].decode()    
+
+def formatStr(string, size):
+    if len(string) < size:
+        string += (size - len(string)) * '\x00'
+    elif len(string) > size:
+        string = string[:size]
+    return string

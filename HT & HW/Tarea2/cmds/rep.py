a66699cdff84b19aaba04c02a7f0b54e35c479a6
andre-corado <98116674+andre-corado@users.noreply.github.com>
2023-09-01 20:54:50 -0600

    Update rep.py

--- HT & HW/Tarea2/cmds/rep.py
+++ HT & HW/Tarea2/cmds/rep.py
@@ -8,30 +8,39 @@ from cmds.structs.MBR import MBR
 
 def execute(consoleLine):
     name, path, id, ruta = '', '', '', ''
+    nameFound, pathFound, idFound, rutaFound = False, False, False, False
     for i in range(len(consoleLine)):
         if consoleLine[i].startswith('-path='):
             path = consoleLine[i][6:]
             if len(path) < 5:
                 return 'Error: Path no puede ser vacío.'
+            pathFound = True
         if consoleLine[i].startswith('-name='):
             name = consoleLine[i][6:].upper()
             if len(name) < 1:
                 return 'Error: Name no puede ser vacío.'
+            nameFound = True
         if consoleLine[i].startswith('-id='):
             id = consoleLine[i][4:]
             if not id.endswith('.dsk') and not id.endswith('.dsk\"'):
                 return 'Error: Id debe ser un archivo .dsk'
             if len(id) < 1:
                 return 'Error: Id no puede ser vacío.'
+            idFound = True
         if consoleLine[i].startswith('-ruta='):
             ruta = consoleLine[i][6:]
             if len(ruta) < 1:
                 return 'Error: Ruta no puede ser vacío.'
+            rutaFound = True
 
     if name == 'MBR':
+        if not pathFound or not idFound:
+            return 'Error: Faltan parámetros obligatorios.'
         return makeMBRTable(path, id)
-
-
+    if name == 'DISK':
+        if not pathFound or not idFound:
+            return 'Error: Faltan parámetros obligatorios.'
+        return makeDiskTable(path, id)
 
 
 def makeMBRTable(tablePath, diskPath):
@@ -68,3 +77,6 @@ def makeMBRTable(tablePath, diskPath):
             return 'Error: Formato de reporte no válido.'
     file.close()
     return 'Tabla MBR creada exitosamente.'
+
+def makeDiskTable(path, id):
+    pass

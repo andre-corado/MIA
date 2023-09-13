04a2c8a5c9bba0978c49f0aefa696e675fbc5609
andre-corado <98116674+andre-corado@users.noreply.github.com>
2023-09-12 20:58:13 -0600

    Create bitmaps.py

---
+++ HT & HW/Tarea2/cmds/rep/bitmaps.py
@@ -0,0 +1,6 @@
+def makebm_inode(superblock, n):
+    try:
+        with open(superblock.path, 'rb+') as file:
+            file.seek(superblock.s_bm_inode_start)
+            file.read(n)
+           
